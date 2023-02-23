ff15f5e09b8cab8c34ff21b4fd8876f61a47daee
NamitQuaraTech <nkumar@quaratech.com>
2023-02-23 18:39:07 +0530

    Flex

--- App.js
+++ App.js
@@ -1,20 +1,192 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet, Button, Image } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View style={{ padding: 50, flexDirection: "row" }}>
+        <View
+          style={{
+            backgroundColor: "#ff0000",
+            width: 50,
+            height: 50,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>1</Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: "#0000ff",
+            width: 50,
+            height: 50,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>2</Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: "#00ff00",
+            width: 50,
+            height: 50,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>3</Text>
+        </View>
+      </View>
+      <View style={{ paddingLeft: 50 }}>
+        <View
+          style={{
+            backgroundColor: "#ff0000",
+            width: 50,
+            height: 50,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>1</Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: "#0000ff",
+            width: 50,
+            height: 50,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>2</Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: "#00ff00",
+            width: 50,
+            height: 50,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>3</Text>
+        </View>
+      </View>
+      <View
+        style={{
+          paddingLeft: 50,
+          paddingTop: 50,
+          flexDirection: "row",
+          width: "80%",
+          height: 300,
+          justifyContent: "space-around",
+          alignItems: "stretch",
+        }}
+      >
+        <View
+          style={{
+            flex:1,
+            backgroundColor: "#ff0000",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>1</Text>
+        </View>
+        <View
+          style={{
+            flex:2,
+            backgroundColor: "#0000ff",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>2</Text>
+        </View>
+        <View
+          style={{
+            flex:3,
+            backgroundColor: "#00ff00",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>3</Text>
+        </View>
+      </View>
+      <View style={{ padding: 50, flexDirection: "row-reverse",justifyContent:"space-evenly" }}>
+        <View
+          style={{
+            backgroundColor: "#ff0000",
+            width: 50,
+            height: 50,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>1</Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: "#0000ff",
+            width: 50,
+            height: 50,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>2</Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: "#00ff00",
+            width: 50,
+            height: 50,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>3</Text>
+        </View>
+      </View>
+      <View style={{ padding: 50, flexDirection: "row-reverse",justifyContent:"space-between" }}>
+        <View
+          style={{
+            backgroundColor: "#ff0000",
+            width: 50,
+            height: 50,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>1</Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: "#0000ff",
+            width: 50,
+            height: 50,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>2</Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: "#00ff00",
+            width: 50,
+            height: 50,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>3</Text>
+        </View>
+      </View>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
